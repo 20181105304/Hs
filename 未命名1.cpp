9507a38d1d3324314f75af45b2603b14162459ed
20181105304 <51770345+20181105304@users.noreply.github.com>
2019-12-13 21:53:03 +0800

    树

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,5 +1,78 @@
-#include<stdio.h>
+#include <stdio.h>
+
+//typedef enum {false, true} bool;
+#define MaxVertexNum 10   /* 最大顶点数设为10 */
+typedef int Vertex;       /* 用顶点下标表示顶点,为整型 */
+
+/* 邻接点的定义 */
+typedef struct AdjVNode *PtrToAdjVNode; 
+struct AdjVNode{
+    Vertex AdjV;        /* 邻接点下标 */
+    PtrToAdjVNode Next; /* 指向下一个邻接点的指针 */
+};
+
+/* 顶点表头结点的定义 */
+typedef struct Vnode{
+    PtrToAdjVNode FirstEdge; /* 边表头指针 */
+} AdjList[MaxVertexNum];     /* AdjList是邻接表类型 */
+
+/* 图结点的定义 */
+typedef struct GNode *PtrToGNode;
+struct GNode{  
+    int Nv;     /* 顶点数 */
+    int Ne;     /* 边数   */
+    AdjList G;  /* 邻接表 */
+};
+typedef PtrToGNode LGraph; /* 以邻接表方式存储的图类型 */
+
+bool Visited[MaxVertexNum]; /* 顶点的访问标记 */
+
+LGraph CreateGraph(); /* 创建图并且将Visited初始化为false；裁判实现，细节不表 */
+
+void Visit( Vertex V )
+{
+    printf(" %d", V);
+}
+
+void BFS ( LGraph Graph, Vertex S, void (*Visit)(Vertex) );
+
 int main()
 {
-	printf("%d",3%7);
- } 
+    LGraph G;
+    Vertex S;
+
+    G = CreateGraph();
+    scanf("%d", &S);
+    printf("BFS from %d:", S);
+    BFS(G, S, Visit);
+
+    return 0;
+}
+
+
+void BFS ( LGraph Graph, Vertex S, void (*Visit)(Vertex) )
+{
+	
+	int a[100],i=0,j=0;
+	Vertex s;
+	PtrToAdjVNode p;
+	
+	Visit(S);
+	Visited[S] = true;
+	
+	a[i++] = S;
+	
+	while(i>j){
+		p = Graph->G[a[j++]].FirstEdge;
+		
+		while(p){
+			s = p->AdjV;
+			if(!Visited[s]){
+				Visit(s);
+	            Visited[s] = true;
+	            a[i++] = s;
+			}
+			p = p->Next;
+		}
+	}	
+}
