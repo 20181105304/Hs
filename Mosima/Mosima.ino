83c282a2a7b89e8557de2773829ff7ba80b7653f
20181105304 <51770345+20181105304@users.noreply.github.com>
2019-12-18 23:55:32 +0800

    摩斯码

--- Mosima/Mosima.ino
+++ Mosima/Mosima.ino
@@ -1,459 +1,228 @@
 int led = 13;
+char str;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led,OUTPUT);
-
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  char ch[26] = {'a'};
-  int i;
-//  scanf("%c",&ch);
-//  for(i=0; ch[i] != '\0'; i++){
-    switch (ch) 
-    {
-      case 'a':
-       msA();
-       break;
-
-
-
-
-
-
-      
-    }
-//  }
-
-//if(ch == 'a'){
-//  msA();
-//}
-
-}
-
-
-void msA(){
-  for(int i=0;i<1;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-}
-
-
-void msB(){
-  for(int i=0;i<1;i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i; i<3; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-}
-
-
-void msC(){
-  for(int i=0;i<1;i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i=0;i<1;i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-}
-
-void msD(){
-  for(int i=0;i<1;i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i; i<2; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-}
-
-void msE(){
-  for(int i=0;i<1;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void loop(){
+  if(Serial.available()){
+    str = Serial.read();
   
-}
-
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
+  switch(str) {
+    case 'a' : msa(); break;
+    case 'b' : msb(); break;
+    case 'c' : msc(); break;
+    case 'd' : msd(); break;
+    case 'e' : mse(); break;
+    case 'f' : msf(); break;
+    case 'g' : msg(); break;
+    case 'h' : msh(); break;
+    case 'i' : msi(); break;
+    case 'j' : msj(); break;
+    case 'k' : msk(); break;
+    case 'l' : msl(); break;
+    case 'm' : msm(); break;
+    case 'n' : msn(); break;
+    case 'o' : mso(); break;
+    case 'p' : msp(); break;
+    case 'q' : msq(); break;
+    case 'r' : msr(); break;
+    case 's' : mss(); break;
+    case 't' : mst(); break;
+    case 'u' : msu(); break;
+    case 'v' : msv(); break;
+    case 'w' : msw(); break;
+    case 'x' : msx(); break;
+    case 'y' : msy(); break;
+    case 'z' : msz(); break;
+    
+    default :  break;
   }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-}
-
-void msG(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
   }
 }
 
-void msH(){
+
+void chang(){
+    digitalWrite(led,HIGH);
+    delay(1000);
+    digitalWrite(led,LOW);
+    delay(1000);
+}
+
+void duan(){
+  digitalWrite(led,HIGH);
+    delay(300);
+    digitalWrite(led,LOW);
+    delay(1000);
+}
+
+
+void msa(){
+  duan();
+  chang();
+}
+
+
+void msb(){
+  chang();
+  duan();
+  duan();
+  duan();
+}
+
+
+void msc(){
+  chang();
+  duan();
+  chang();
+  duan();
+}
+
+void msd(){
+  chang();
+  duan();
+  duan();
+}
+
+void mse(){
+  duan();
+}
+
+void msf(){
+  duan();
+  duan();
+  chang();
+  duan();
+}
+
+void msg(){
+  chang();
+  chang();
+  duan();
+}
+
+void msh(){
   for(int i=0;i<4;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
+    duan();
   }
 }
 
 
-void msI(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msi(){
+  duan();
+  duan();
 
 }
 
-void msJ(){
-  for(int i=0;i<1;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<3; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  
+void msj(){
+  duan();
+  chang();
+  chang();
+  chang();
 }
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msk(){
+  chang();
+  duan();
+  chang();
 }
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msl(){
+  duan();
+  chang();
+  duan();
+  duan();
 }
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msm(){
+  chang();
+  chang();
 }
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msn(){
+  chang();
+  duan();
 }
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void mso(){
+  chang();
+  chang();
+  chang();
 }
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msp(){
+  duan();
+  chang();
+  chang();
+  duan();
 }
 
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msq(){
+  chang();
+  chang();
+  duan();
+  chang();
+}
 
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
+void msr(){
+  duan();
+  chang();
+  duan();
+}
 
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void mss(){
+  duan();
+  duan();
+  duan();
+}
+
+void mst(){
+  chang();
+}
+
+void msu(){
+  duan();
+  duan();
+  chang();
 }
 
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
+void msv(){
+  for(int i=0;i<3;i++){
+    duan();
   }
+  chang();
 
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
+}
 
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msw(){
+  duan();
+  chang();
+  chang();
 }
 
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msx(){
+  chang();
+  duan();
+  duan();
+  chang();
 }
 
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msy(){
+  chang();
+  duan();
+  chang();
+  chang();
 }
 
-
-void msF(){
-  for(int i=0;i<2;i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
-
-  for(int i; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(1000);
-    digitalWrite(led,LOW);
-    delay(1000);
-  }
-
-  for(int i=0; i<1; i++){
-    digitalWrite(led,HIGH);
-    delay(300);
-    digitalWrite(led,LOW);
-    delay(300);
-  }
+void msz(){
+  chang();
+  chang();
+  duan();
+  duan();
 }
